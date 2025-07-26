429382144fb781c88dab704881d7c98a3eafb49e
Wellington-Gelado <wellingtonsantos1996@usp.br>
2025-07-26 17:33:29 -0300

    Adequando aplicação para rodar como 'app'.

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'wellington-offline-cache-v1';
+const OFFLINE_URL = '/offline.html';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.add(OFFLINE_URL);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+    );
+  }
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
